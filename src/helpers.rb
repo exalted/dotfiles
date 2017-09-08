4ea1996af4b09b31e1aa27c67cdfe883d90caaec
Ali Servet Donmez <asd@pittle.org>
2017-09-08 15:45:11 +0200

    Moves configuration files from config/ to dotfiles/

--- src/helpers.rb
+++ src/helpers.rb
@@ -13,5 +13,5 @@ def link(from, to)
   dirname = Pathname.new(to_path).dirname.to_s
   system "/bin/mkdir", "-p", dirname unless Dir.exists?(dirname)
 
-  system "ln -sf #{__dir__}/../config/#{from} #{to_path}"
+  system "ln -sf #{__dir__}/../dotfiles/#{from} #{to_path}"
 end
