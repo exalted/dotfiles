03d788b6b3990d7fae9df1b3c4061c83d0dc482a
Ali Servet Donmez <asd@pittle.org>
2017-09-10 16:29:11 +0200

    Use `mas` to install applications from App Store

--- src/steps/install_homebrew.rb
+++ src/steps/install_homebrew.rb
@@ -1,6 +1,10 @@
 require_relative '../common'
+require_relative '../helpers'
 
 def install_homebrew
-  return if cmd_exists('brew')
-  system "/usr/bin/ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
+  unless cmd_exists('brew')
+    system "/usr/bin/ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
+  end
+
+  brew 'mas'
 end
