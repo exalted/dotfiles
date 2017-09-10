3db3a5894dd30589d2eddfa4d39481391973ed61
Ali Servet Donmez <asd@pittle.org>
2017-09-10 19:01:06 +0200

    Slightly better  homebrew installation

--- src/steps/install_homebrew.rb
+++ src/steps/install_homebrew.rb
@@ -2,9 +2,8 @@ require_relative '../common'
 require_relative '../helpers'
 
 def install_homebrew
-  unless cmd_exists('brew')
-    system "/usr/bin/ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
-  end
-
+  return if cmd_exists('brew')
+  system "/usr/bin/ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
+  system 'brew tap caskroom/cask'
   brew 'mas'
 end
