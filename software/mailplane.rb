76b9822c022bc5db2a35f1f4e5e62c585ed2fdb8
Ali Servet Donmez <asd@pittle.org>
2018-09-18 12:23:25 +0200

    Mailplane 3 is not available anymore
    … since Mailplane 4 was released

--- software/mailplane.rb
+++ software/mailplane.rb
@@ -1,6 +1,7 @@
 #!/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby
 require_relative '../src/helpers'
 
-cask 'mailplane'
+# TODO: Since the release of v4, there's no cask for v3 anymore…
+# cask 'mailplane'
 
 # TODO: consider saving its settings
