ec72dec0fff17a041b65373e138d2595dbefd93a
Ali Servet Donmez <asd@pittle.org>
2018-10-15 15:15:57 +0200

    Disable installing Sketch via cask
    … since my license is expired, I don't want to upgrade it to latest

--- software/sketch.rb
+++ software/sketch.rb
@@ -1,4 +1,6 @@
 #!/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby
 require_relative '../src/helpers'
 
-cask 'sketch'
+## My license expired, so the latest version I can use (43.2) can be downloaded
+## from https://www.sketchapp.com/updates/?expiration_date=1492258332
+# cask 'sketch'
