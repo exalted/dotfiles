02f97156be59cff96da86de603628a240ad14af1
Ali Servet Donmez <asd@pittle.org>
2018-10-27 20:40:32 +0200

    Unmount & remount all external disks with keyboard shortcuts

---
+++ software/custom_automator/remountall.rb
@@ -0,0 +1,11 @@
+#!/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby
+require_relative '../../src/helpers'
+
+# TODO: The technique below is copied from `software/mosaic.rb`, no bueno…
+
+filename = "Remount all.workflow"
+src = "#{__dir__}/#{filename}"
+dest = "#{ENV["HOME"]}/Library/Services"
+
+FileUtils.mkdir_p dest
+FileUtils.cp_r(src, dest, preserve: true)
