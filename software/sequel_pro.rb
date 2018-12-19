07dabd22738ff7986c6c1a7ceb2ba1de8005a3b3
Ali Servet Donmez <asd@pittle.org>
2018-12-19 13:20:26 +0100

    Install Sequel Pro nightly

--- software/sequel_pro.rb
+++ software/sequel_pro.rb
@@ -1,6 +1,10 @@
 #!/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby
 require_relative '../src/helpers'
 
-cask 'sequel-pro'
+# We are installing nightly, because for whatever reason Sequel Pro didn't
+# release a new version in years, but still fixing bugs and stuff and building
+# nightlies :shrug:
+cask 'sequel-pro-nightly'
+# cask 'sequel-pro'
 
 # TODO: consider saving its settings
