d363789dbf301c0258a5c24b318fdda52be5bc26
Ali Servet Donmez <asd@pittle.org>
2019-05-06 12:56:46 +0200

    Fix convox install

--- software/convox/convox.rb
+++ software/convox/convox.rb
@@ -3,7 +3,7 @@ require_relative '../../src/helpers'
 
 unless cmd_exists?('convox')
   # https://convox.com/docs/installation/#os-x
-  Kernel.system "curl -fsSL https://convox.com/cli/osx/convox -o /tmp/convox"
+  Kernel.system "curl -fsSL https://convox.com/cli/macos/convox -o /tmp/convox"
   Kernel.system "mv /tmp/convox /usr/local/bin/convox"
   Kernel.system "chmod 755 /usr/local/bin/convox"
 end
