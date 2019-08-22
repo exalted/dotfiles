39d601a3e9ccc373b9b75284d2665f6ebf2b66c9
Ali Servet Donmez <asd@pittle.org>
2019-08-22 18:17:31 +0200

    Update finicky configuration

--- software/finicky/finicky.js
+++ software/finicky/finicky.js
@@ -1,13 +1,51 @@
+const zoomLinkFromGCal = /https?:\/\/(.+\.)?google\.com\/url\?q=https?:\/\/zoom\.us(\/.+?)&/;
+
 module.exports = {
+  options: {
+    // Hide the finicky icon from the top bar
+    hideIcon: false
+  },
   defaultBrowser: 'Safari',
+  rewrite: [
+    // Redirect all urls to use https
+    {
+      match: ({ url }) => url.host !== 'localhost',
+      url: ({ url }) => ({ ...url, protocol: 'https' })
+    },
+    // Extract zoom meeting link when clicked from Mailplane
+    {
+      match: ({ sourceBundleIdentifier, urlString }) => {
+        if (sourceBundleIdentifier !== 'com.mailplaneapp.Mailplane3') { return false }
+
+        return urlString.match(zoomLinkFromGCal);
+      },
+      url: ({ urlString }) => {
+        const matches = urlString.match(zoomLinkFromGCal);
+
+        return {
+          protocol: 'https',
+          host: 'zoom.us',
+          pathname: matches[2]
+        };
+      }
+    }
+  ],
   handlers: [
+    {
+      match: finicky.matchDomains([ 'localhost' ]),
+      browser: 'Google Chrome'
+    },
     {
       match: finicky.matchDomains([ /(.+\.)?pivotaltracker\.com/ ]),
       browser: 'Pivotal Tracker'
     },
     {
-      match: finicky.matchDomains([ /(plus|hangouts)\.google\.com/ ]),
+      match: finicky.matchDomains([ /(plus|hangouts|meet)\.google\.com/ ]),
       browser: 'Google Chrome'
+    },
+    {
+      match: finicky.matchDomains([ /(.+\.)?zoom\.us/ ]),
+      browser: 'us.zoom.xos'
     }
   ]
 };
