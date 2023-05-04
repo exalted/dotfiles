f619f52cde5cc5fad0c1786db22c5851782026b9
Ali Servet Donmez <asd@pittle.org>
2023-05-04 10:33:44 +0200

    Make sure sam CLI works

--- software/aws/aws.rb
+++ software/aws/aws.rb
@@ -1,4 +1,7 @@
 #!/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby
-require_relative '../src/helpers'
+require_relative '../../src/helpers'
 
 brew 'awscli'
+# TODO: install AWS SAM CLI
+
+bash_source_relative
