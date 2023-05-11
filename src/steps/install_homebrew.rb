18aa76f0f125646ad13ab4b057328516a76ac049
Ali Servet Donmez <asd@pittle.org>
2023-05-11 14:58:35 +0200

    Prefer single-quote'd strings to double-quotes…
    … when there's no string interpolation taking place

--- src/steps/install_homebrew.rb
+++ src/steps/install_homebrew.rb
@@ -4,7 +4,7 @@ require_relative '../helpers'
 def install_homebrew
   # Don't attempt to install if already installed
   return if cmd_exists?('brew')
-  system "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+  system '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
   system 'brew tap homebrew/cask-versions'
   system 'brew tap homebrew/cask-drivers'
 end
