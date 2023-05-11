a4a6c3f62a963bdca36aa575de42b42619c29fb8
Ali Servet Donmez <asd@pittle.org>
2023-05-11 14:57:44 +0200

    Fix nvm installation

--- software/node/node.rb
+++ software/node/node.rb
@@ -13,8 +13,10 @@ brew 'node@12'
 # Node Version Manager
 #
 brew 'nvm'
-Kernel.system "mkdir -p #{ENV["HOME"]}/.nvm"
-Kernel.system "nvm alias default system"  # make sure nvm to not "interfere" unless explicitely `nvm use`'d
+system "mkdir -p #{ENV["HOME"]}/.nvm"
+# See `brew info nvm` to learn about how to load nvm:
+LOAD_NVM_CMD = 'export NVM_DIR="$HOME/.nvm" && [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"'
+system "#{LOAD_NVM_CMD} && nvm alias default system" # make sure nvm to not "interfere" unless explicitely `nvm use`'d
 
 link_to_home_relative 'npmrc', '.npmrc'
 
