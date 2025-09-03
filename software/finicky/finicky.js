8bb1d75fd74d7878bf5fbeab0e7eb1264f71fd17
Ali Servet Donmez <asd@pittle.org>
2025-09-03 11:27:13 +0200

    Updates

--- software/finicky/finicky.js
+++ software/finicky/finicky.js
@@ -1,62 +1,28 @@
-const console = {
-  log: finicky.log,  // so that you can `console.log` like you're used to
-};
+// @ts-check
 
-// const zoomLinkFromGCal = /https?:\/\/(.+\.)?google\.com\/url\?q=https?:\/\/(.+\.)?zoom\.us(\/[^&]+)/;
-// const googleVideoCallLinkFromGCal = /https?:\/\/(.+\.)?google\.com\/url\?q=https?:\/\/((chrome|plus|hangouts|meet)\.google\.com\/.+?)&/;
+/**
+ * @typedef {import('/Applications/Finicky.app/Contents/Resources/finicky.d.ts').FinickyConfig} FinickyConfig
+ */
 
-module.exports = {
-  options: {
-    // Hide the finicky icon from the top bar
-    hideIcon: true,
-  },
+/**
+ * @type {FinickyConfig}
+ */
+export default {
   defaultBrowser: 'Safari',
   rewrite: [
     // Redirect all urls to use https
     {
-      match: ({ url }) => (
+      match: (url) => (
         url.protocol === 'http' &&
         url.host !== 'localhost' &&
         url.host !== '127.0.0.1'
       ),
-      url: ({ url }) => ({ ...url, protocol: 'https' }),
+      url: (url) => ({ ...url, protocol: 'https' }),
     },
-    // // Extract zoom meeting link when clicked from Mailplane
-    // {
-    //   match: ({ opener: { bundleId }, urlString }) => {
-    //     if (bundleId !== 'com.mailplaneapp.Mailplane3') {
-    //       return false;
-    //     }
-
-    //     return zoomLinkFromGCal.test(decodeURIComponent(urlString));
-    //   },
-    //   url: ({ urlString }) => {
-    //     const matches = zoomLinkFromGCal.exec(decodeURIComponent(urlString));
-    //     const pathname = matches[3];
-
-    //     return `https://zoom.us${pathname}`;
-    //   },
-    // },
-    // Extract video call link for various Google services when clicked from Mailplane
-    // {
-    //   match: ({ opener: { bundleId }, urlString }) => {
-    //     if (bundleId !== 'com.mailplaneapp.Mailplane3') {
-    //       return false;
-    //     }
-
-    //     return googleVideoCallLinkFromGCal.test(decodeURIComponent(urlString));
-    //   },
-    //   url: ({ urlString }) => {
-    //     const matches = googleVideoCallLinkFromGCal.exec(decodeURIComponent(urlString));
-    //     const match = matches[2];
-
-    //     return `https://${match}`;
-    //   },
-    // },
   ],
   handlers: [
     {
-      match: finicky.matchHostnames(['localhost', '127.0.0.1', /\.local$/, /\.convox$/]),
+      match: finicky.matchHostnames(['localhost', '127.0.0.1', /\.local$/]),
       browser: {
         name: 'Google Chrome',
         profile: "Default",
@@ -73,11 +39,5 @@ module.exports = {
       match: finicky.matchHostnames([/(.+\.)?zoom\.us/]),
       browser: 'us.zoom.xos',
     },
-    // {
-    //   match: ({ urlString }) => {
-    //     return /https:\/\/balsamiq\.atlassian\.net\/(jira|browse\/[A-Z]+-[0-9]+)/.test(urlString);
-    //   },
-    //   browser: 'Balsamiq Jira',
-    // },
   ],
 };
