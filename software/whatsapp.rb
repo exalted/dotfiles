1d9a69e6d796fcc50ed1ef46820ce7f6117cd3d8
Ali Servet Donmez <asd@pittle.org>
2026-07-24 10:43:40 +0200

    Use AppStore version of WhatsApp to attempt to fix sync issues

--- software/whatsapp.rb
+++ software/whatsapp.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
 require_relative '../src/helpers'
 
-cask 'whatsapp'
+# 23 Jul 2026: Cask version of WhatsApp wasn't sync-ing correctly, so I switched back to AppStore version.
+# cask 'whatsapp'
+mas '310633997' # WhatsApp
